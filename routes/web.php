7c988730528ec6c960037e95fa2500f2cfb6e38e
Ferry Firmansyah <ferryfirmansyah404@gmail.com>
2023-12-17 16:31:16 +0700

    Refactor logout routes and add authentication middleware to dashboard route

--- routes/web.php
+++ routes/web.php
@@ -25,10 +25,6 @@ Route::post('/postRegist', [LoginController::class, 'postRegist']);
 
 Route::post('/postLogin', [LoginController::class, 'postLogin']);
 
-Route::get('/logout', [LoginController::class, 'logout']);
-Route::get('/{any?}/logout', [LoginController::class, 'logout'])
-    ->where('any', '.*');
-
 Route::get('/about', function () {
     return view('pages.about');
 });
@@ -47,6 +43,10 @@ Route::get('/topup', function () {
 
 Route::group(['middleware' => ['auth']], function () {
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+
+    Route::get('/logout', [LoginController::class, 'logout']);
+    Route::get('/{any?}/logout', [LoginController::class, 'logout'])
+        ->where('any', '.*');
 
     Route::get('/order', function () {
         return view('pages.customers.order.laundry');
